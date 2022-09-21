9273c96513ae73377f4014418c9f474316c7f452
RomanUstychenko <roman.ustychenko@gmail.com>
2022-09-21 08:27:20 +0300

    upd feedback

--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -1,25 +1,40 @@
+import { Component} from "react"
 import css from "./Feedback.module.css"
 
 
-// state = {
-//   good: 0,
-//   neutral: 0,
-//   bad: 0
-// }
 
-export const Feedback = () => {
+
+export default class Feedback extends Component  {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+      }
+
+      leaveVote = (propertyName) => {
+        this.setState((prev) => {
+            const value = prev[propertyName];
+            return {
+                [propertyName]: value + 1
+            }
+        })
+      }
+    render () {
+        const {good, neutral, bad} = this.state;
     return (
         <div className={css.feedbackList}>
             <p className={css.feedbackTitle}>Please leave feedback</p>
-            <button>Good</button>
-            <button>Neutral</button>
-            <button>Bad</button>
-            <p>Statistics</p>
-            <ul>
-                <li>Good:</li>
-                <li>Neutral:</li>
-                <li>Bad:</li>
+            <button onClick={() => this.leaveVote("good")} className={css.feedbackBtn}>Good</button>
+            <button onClick={() => this.leaveVote("neutral")} className={css.feedbackBtn}>Neutral</button>
+            <button onClick={() => this.leaveVote("bad")} className={css.feedbackBtn}>Bad</button>
+            <p className={css.statisticsTitle}>Statistics</p>
+            <ul className={css.statisticsList}>
+                <li className={css.statisticsItem}>Good: {good}</li>
+                <li className={css.statisticsItem}>Neutral: {neutral}</li>
+                <li className={css.statisticsItem}>Bad: {bad}</li>
             </ul>
         </div>
     )
 }
+}
+// 2.08
