850aa43a1e743d99330a01c59c63a44368274f54
RomanUstychenko <roman.ustychenko@gmail.com>
2022-10-12 19:31:45 +0300

    Update Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,17 +1,14 @@
-// import { Component} from "react";
 import SectionTitle from "./SectionTitle/SectionTitle";
 import Statistics from "./Statistics/Statistics"
 import FeedbackOptions from "./FeedbackOptions/FeedbackOptions"
 import Notification from "./Notification/Notification"
 import { useState } from "react";
-// import React from 'react'
 
 export const Feed = () => {
 
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
-
     const optionsFeed = { good, neutral, bad };
 
    const countTotalFeedback = () => {
@@ -36,7 +33,6 @@ export const Feed = () => {
             return;
         }
       }
-      
       const btn = Object.keys(optionsFeed);
 
     return (
